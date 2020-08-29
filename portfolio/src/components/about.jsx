759062e64286976cfedde2ec47f6b84bc4de8737
Landon Germain Tipantiza <landontipantiza@Landons-MacBook-Pro.local>
2020-08-29 13:11:57 -0600

    formatted to fit the way i want main div

--- portfolio/src/components/about.jsx
+++ portfolio/src/components/about.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 export default function About(){
+
   return (
     <>
-      <div style={{height:"100vh", width:"100vw"}}>
-
+      <div style={{margin:"5vh 10vh", height:"100vh", display:"flex", flexDirection:"column",  alignItems:'center'}}>
+        <h1>
+          About
+        </h1>
+        <img src="" alt=""/>
+        <p>
+          landon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skf landon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skf landon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skflandon slkas sdkfjs d sdkfjsdf,sdfk slkfjsd skd aksdf laskdfndkf, alskd.s dfskjjdf adk sdkf skf
+        </p>
       </div>
       
     </>
