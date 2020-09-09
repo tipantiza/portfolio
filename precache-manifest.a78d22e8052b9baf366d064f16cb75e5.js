99bc5579d3d00f6a7d2885ca6240c8c3e66c2c79
Landon Germain Tipantiza <landontipantiza@landons-mbp.gateway.sasktel.net>
2020-09-08 20:49:59 -0600

    Updates

---
+++ precache-manifest.a78d22e8052b9baf366d064f16cb75e5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c67ebd8b54b6be625cd8a3296a0ea32",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f8573f549c2e7c2730d9",
+    "url": "/portfolio/static/css/main.bdc9828a.chunk.css"
+  },
+  {
+    "revision": "16e2a8b49d0656875c62",
+    "url": "/portfolio/static/js/2.1a497eef.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.1a497eef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8573f549c2e7c2730d9",
+    "url": "/portfolio/static/js/main.93fa3118.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "3df23f5df64f3983a777e5294b1b3b4e",
+    "url": "/portfolio/static/media/Screen Shot 2020-09-02 at 1.13.17 PM.3df23f5d.png"
+  },
+  {
+    "revision": "28e64c84d190af2be69a49730f42ec6b",
+    "url": "/portfolio/static/media/atlasJams.28e64c84.png"
+  },
+  {
+    "revision": "0fc9d143732cd11d6fbeb4e2fcbc07f0",
+    "url": "/portfolio/static/media/css.0fc9d143.png"
+  },
+  {
+    "revision": "fef702d6be16503c5950a313e70f2817",
+    "url": "/portfolio/static/media/git.fef702d6.png"
+  },
+  {
+    "revision": "00dc622157af87a42dad36d794bb0747",
+    "url": "/portfolio/static/media/html.00dc6221.png"
+  },
+  {
+    "revision": "50a67f1cbe0f46b4cd999a8d083155bc",
+    "url": "/portfolio/static/media/javascriptLogo.50a67f1c.png"
+  },
+  {
+    "revision": "a5ed0dca4e0e57ec5f043ce4ade57e2c",
+    "url": "/portfolio/static/media/landon.a5ed0dca.jpg"
+  },
+  {
+    "revision": "521444a28d1ac7003ece0c1f030dcd21",
+    "url": "/portfolio/static/media/node.521444a2.png"
+  },
+  {
+    "revision": "8ab12c496de37bfd940ae032c64aafa4",
+    "url": "/portfolio/static/media/react.8ab12c49.png"
+  },
+  {
+    "revision": "f7af5ef5c3778bf43f770e26c86ec4cd",
+    "url": "/portfolio/static/media/scheduler.f7af5ef5.png"
+  },
+  {
+    "revision": "c8b7d5b1ff4b71d163057e04661ccfda",
+    "url": "/portfolio/static/media/slightlyBadDirections.c8b7d5b1.png"
+  },
+  {
+    "revision": "680e59931c504874ff072d0ac342c9bd",
+    "url": "/portfolio/static/media/sql.680e5993.png"
+  },
+  {
+    "revision": "b2439111ed890cf69225fbabcd2fe5c4",
+    "url": "/portfolio/static/media/toronto_cn.b2439111.jpg"
+  },
+  {
+    "revision": "41534d68b4634e3844fe111ff8cdf20a",
+    "url": "/portfolio/static/media/tweeter.41534d68.png"
+  }
+]);
